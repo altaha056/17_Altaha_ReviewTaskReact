684c96ecb7b574f007865e548e6a2bbcfa5db777
Altaha <altahabinsyafriadi@gmail.com>
2021-09-19 23:16:33 +0700

    commit ketiga

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./store/reducers";
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
